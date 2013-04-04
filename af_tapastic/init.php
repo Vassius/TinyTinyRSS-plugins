196a9c73744abd2d21b87c0c8b8e0a9c55d836e4
Markus Wiik <vassius@gmail.com>
2013-04-04 14:44:24 +0000

    Fix whitespace

--- af_tapastic/init.php
+++ af_tapastic/init.php
@@ -1,26 +1,26 @@
 <?php
 class Af_Tapastic extends Plugin {
 
-	function about() {
-		return array(0.1, "Fetch image from comics hosted at tapastic.com", "Markus Wiik");
-	}
+    function about() {
+        return array(0.1, "Fetch image from comics hosted at tapastic.com", "Markus Wiik");
+    }
 
-	function init($host) {
-		$host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
-	}
+    function init($host) {
+        $host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
+    }
 
-	function hook_article_filter($article) {
-		$owner_uid = $article["owner_uid"];
+    function hook_article_filter($article) {
+        $owner_uid = $article["owner_uid"];
 
-		if (strpos($article["link"], "tapastic.com") !== FALSE) {
-			if (strpos($article["plugin_data"], "tapastic,$owner_uid:") === FALSE) {
+        if (strpos($article["link"], "tapastic.com") !== FALSE) {
+            if (strpos($article["plugin_data"], "tapastic,$owner_uid:") === FALSE) {
 
-				$doc = new DOMDocument();
-				$doc->loadHTML(fetch_file_contents($article["link"]));
+                $doc = new DOMDocument();
+                $doc->loadHTML(fetch_file_contents($article["link"]));
 
-				if ($doc) {
-					$xpath = new DOMXPath($doc);
-					$imgnode = $xpath->query('//img[@class="art-image"]')->item(0);
+                if ($doc) {
+                    $xpath = new DOMXPath($doc);
+                    $imgnode = $xpath->query('//img[@class="art-image"]')->item(0);
                     if (is_null($imgnode)) {
                         return $article;
                     }
@@ -28,11 +28,11 @@ class Af_Tapastic extends Plugin {
                     $article["plugin_data"] = "tapastic,$owner_uid:" . $article["plugin_data"];
                 }
             }
-			else if (isset($article["stored"]["content"])) {
-				$article["content"] = $article["stored"]["content"];
-			}
+            else if (isset($article["stored"]["content"])) {
+                $article["content"] = $article["stored"]["content"];
+            }
         }
-		return $article;
-	}
+        return $article;
+    }
 }
 ?>
